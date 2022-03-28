2efa2449c42bc0fffc353f66c461fea214df4c6d
Inglish Foust <inglish.foust5@gmail.com>
2022-03-28 16:02:05 -0400

    changing js

---
+++ assets/js/test-question.js
@@ -0,0 +1,98 @@
+//creating test questions
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Test Processing",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Choose the correct HTML element for the largest heading:",
+        answer: "h1",
+        options: [
+            "head",
+            "heading",
+            "h1",
+            "h5"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does CSS stand for?",
+        answer: "Cascading Stylesheet",
+        options: [
+            "Colorful Stylesheet",
+            "Computing Stylesheet",
+            "Cascading Stylesheet",
+            "Common Stylesheet"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What is the correct syntax for referring to an external script called xxx.js?",
+        answer: "script src='xxx.js'",
+        options: [
+            "script name='xxx.js'",
+            "script href='xxx.js'",
+            "script 'xxx.js'",
+            "script src='xxx.js'"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Where in an HTML document is the correct place to refer to an external style sheet?",
+        answer: "In the head section",
+        options: [
+            "At the end of the document",
+            "In the body section",
+            "In the head section",
+            "At the very top"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Which is the correct CSS syntax?",
+        answer: "body:color=black",
+        options: [
+            "{body:color=black;}",
+            "body:color=black",
+            "{body:color:black}",
+            "body (color:black)"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "alert('Hello World')",
+        options: [
+            "alert('Hello World')",
+            "alertBox('Hello World')",
+            "msg('Hello World')",
+            "msgBox('Hello World')"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "How do you add a background color for all <h1> elements?",
+        answer: "h1 {background-color:#fffff;}",
+        options: [
+            "all.h1  {background-color:#fffff;}",
+            "h1.all {background-color:#fffff;}",
+            "h1 (background:#fffff)",
+            "h1 {background-color:#fffff;}"
+        ]
+    },
+];
